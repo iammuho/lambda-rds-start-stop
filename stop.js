ccf120e24b9c55209f7a2a2fe7ad4c57cffff141
sdebacker <sam.de.backer@persgroep.be>
2018-06-28 15:57:34 +0200

    Added option to no take a snapshot on

--- stop.js
+++ stop.js
@@ -1,12 +1,12 @@
 var AWS = require('aws-sdk');
 
-module.exports = (instanceId) => {
+module.exports = (instanceId, snapshot) => {
     var rds = new AWS.RDS();
     var today = new Date();
     var params = {
-      DBInstanceIdentifier: instanceId, 
-      DBSnapshotIdentifier: `${instanceId}-${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}-${today.getTime()}`
+      DBInstanceIdentifier: instanceId
     };
+    if (snapshot) params["DBSnapshotIdentifier"] = `${instanceId}-${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}-${today.getTime()}`;
     rds.stopDBInstance(params, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
       else     console.log(data);           // successful response
